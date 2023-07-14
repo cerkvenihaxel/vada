0698729e218aab1ac56ca12e50c3ad801f0316af
cerkvenihaxel <cerkvenihaxel@gmail.com>
2023-07-14 13:37:21 -0300

    First commit

---
+++ app/Http/Livewire/LoginMidone.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LoginMidone extends Component
+{
+    public function render()
+    {
+        return view('livewire.login-midone');
+    }
+}
